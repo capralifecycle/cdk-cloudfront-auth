e57f3c473e85c714c3b5443826dce0313ddc6629
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-11-27 14:01:07 +0100

    feat: migrate to esm

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,12 @@
 // Override values in CDK stacks during tests.
 process.env.IS_SNAPSHOT = "true"
 
-module.exports = {
+export default {
   testMatch: ["**/*.test.ts"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    "\\.html$": "<rootDir>/file-mock.js",
+    "\\.html$": "<rootDir>/file-mock.cjs",
   },
 }
